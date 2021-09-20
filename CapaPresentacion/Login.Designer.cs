1769881dd61b4f5ead9b4c5d4c6a7d31a88d2517
basto <75378141+eracles99@users.noreply.github.com>
2021-09-20 17:47:01 -0500

    wwww

--- CapaPresentacion/Login.Designer.cs
+++ CapaPresentacion/Login.Designer.cs
@@ -32,32 +32,28 @@ namespace CapaPresentacion
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
             this.gunaElipse1 = new Guna.UI.WinForms.GunaElipse(this.components);
-            this.bunifuElipse1 = new Bunifu.Framework.UI.BunifuElipse(this.components);
-            this.bunifuElipse2 = new Bunifu.Framework.UI.BunifuElipse(this.components);
             this.label4 = new System.Windows.Forms.Label();
             this.bunifuElipse3 = new Bunifu.Framework.UI.BunifuElipse(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.btnCerrar = new System.Windows.Forms.PictureBox();
             this.panel2 = new System.Windows.Forms.Panel();
-            this.bunifuElipse4 = new Bunifu.Framework.UI.BunifuElipse(this.components);
-            this.gunaElipse2 = new Guna.UI.WinForms.GunaElipse(this.components);
-            this.gunaElipse3 = new Guna.UI.WinForms.GunaElipse(this.components);
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.lblError = new System.Windows.Forms.Label();
             this.panel4 = new System.Windows.Forms.Panel();
-            this.btnIniciar = new Bunifu.Framework.UI.BunifuFlatButton();
-            this.btnCerrar = new System.Windows.Forms.PictureBox();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.pictureBox3 = new System.Windows.Forms.PictureBox();
             this.panel3 = new System.Windows.Forms.Panel();
             this.labelContraseña = new System.Windows.Forms.Label();
             this.labelUsuario = new System.Windows.Forms.Label();
             this.txtcontraseña = new System.Windows.Forms.TextBox();
             this.txtusuario = new System.Windows.Forms.TextBox();
+            this.gunaElipse2 = new Guna.UI.WinForms.GunaElipse(this.components);
+            this.btnIniciar = new System.Windows.Forms.Button();
+            this.gunaElipse3 = new Guna.UI.WinForms.GunaElipse(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.btnCerrar)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             this.panel3.SuspendLayout();
             this.SuspendLayout();
@@ -66,15 +62,6 @@ namespace CapaPresentacion
             // 
             this.gunaElipse1.Radius = 2;
             this.gunaElipse1.TargetControl = this;
-            // 
-            // bunifuElipse1
-            // 
-            this.bunifuElipse1.ElipseRadius = 10;
-            this.bunifuElipse1.TargetControl = this.btnIniciar;
-            // 
-            // bunifuElipse2
-            // 
-            this.bunifuElipse2.ElipseRadius = 7;
             // 
             // label4
             // 
@@ -89,6 +76,14 @@ namespace CapaPresentacion
             this.bunifuElipse3.ElipseRadius = 11;
             this.bunifuElipse3.TargetControl = this.pictureBox1;
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            resources.ApplyResources(this.pictureBox1, "pictureBox1");
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.SeaShell;
@@ -98,75 +93,6 @@ namespace CapaPresentacion
             resources.ApplyResources(this.panel1, "panel1");
             this.panel1.Name = "panel1";
             // 
-            // panel2
-            // 
-            resources.ApplyResources(this.panel2, "panel2");
-            this.panel2.Name = "panel2";
-            // 
-            // bunifuElipse4
-            // 
-            this.bunifuElipse4.ElipseRadius = 10;
-            this.bunifuElipse4.TargetControl = this;
-            // 
-            // gunaElipse2
-            // 
-            this.gunaElipse2.Radius = 2;
-            // 
-            // gunaElipse3
-            // 
-            this.gunaElipse3.Radius = 2;
-            // 
-            // groupBox1
-            // 
-            resources.ApplyResources(this.groupBox1, "groupBox1");
-            this.groupBox1.ForeColor = System.Drawing.Color.White;
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.TabStop = false;
-            // 
-            // lblError
-            // 
-            resources.ApplyResources(this.lblError, "lblError");
-            this.lblError.BackColor = System.Drawing.Color.Transparent;
-            this.lblError.ForeColor = System.Drawing.Color.Red;
-            this.lblError.Name = "lblError";
-            // 
-            // panel4
-            // 
-            resources.ApplyResources(this.panel4, "panel4");
-            this.panel4.Name = "panel4";
-            // 
-            // btnIniciar
-            // 
-            this.btnIniciar.Activecolor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.btnIniciar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(81)))), ((int)(((byte)(124)))), ((int)(((byte)(155)))));
-            resources.ApplyResources(this.btnIniciar, "btnIniciar");
-            this.btnIniciar.BorderRadius = 0;
-            this.btnIniciar.ButtonText = " Iniciar ";
-            this.btnIniciar.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.btnIniciar.DisabledColor = System.Drawing.Color.Gray;
-            this.btnIniciar.ForeColor = System.Drawing.Color.Maroon;
-            this.btnIniciar.Iconcolor = System.Drawing.Color.Transparent;
-            this.btnIniciar.Iconimage = ((System.Drawing.Image)(resources.GetObject("btnIniciar.Iconimage")));
-            this.btnIniciar.Iconimage_right = null;
-            this.btnIniciar.Iconimage_right_Selected = null;
-            this.btnIniciar.Iconimage_Selected = null;
-            this.btnIniciar.IconMarginLeft = 0;
-            this.btnIniciar.IconMarginRight = 0;
-            this.btnIniciar.IconRightVisible = true;
-            this.btnIniciar.IconRightZoom = 0D;
-            this.btnIniciar.IconVisible = true;
-            this.btnIniciar.IconZoom = 90D;
-            this.btnIniciar.IsTab = false;
-            this.btnIniciar.Name = "btnIniciar";
-            this.btnIniciar.Normalcolor = System.Drawing.Color.FromArgb(((int)(((byte)(81)))), ((int)(((byte)(124)))), ((int)(((byte)(155)))));
-            this.btnIniciar.OnHovercolor = System.Drawing.Color.DarkTurquoise;
-            this.btnIniciar.OnHoverTextColor = System.Drawing.Color.Black;
-            this.btnIniciar.selected = false;
-            this.btnIniciar.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnIniciar.Textcolor = System.Drawing.Color.White;
-            this.btnIniciar.TextFont = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnIniciar.Click += new System.EventHandler(this.btnIniciar_Click);
-            // 
             // btnCerrar
             // 
             this.btnCerrar.Image = global::CapaPresentacion.Properties.Resources.cerrar;
@@ -174,6 +100,11 @@ namespace CapaPresentacion
             this.btnCerrar.Name = "btnCerrar";
             this.btnCerrar.TabStop = false;
             this.btnCerrar.Click += new System.EventHandler(this.btnCerrar_Click);
+            // 
+            // panel2
+            // 
+            resources.ApplyResources(this.panel2, "panel2");
+            this.panel2.Name = "panel2";
             // 
             // pictureBox2
             // 
@@ -187,13 +118,17 @@ namespace CapaPresentacion
             this.pictureBox2.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox2_MouseMove);
             this.pictureBox2.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox2_MouseUp);
             // 
-            // pictureBox1
+            // lblError
             // 
-            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
-            resources.ApplyResources(this.pictureBox1, "pictureBox1");
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.TabStop = false;
-            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            resources.ApplyResources(this.lblError, "lblError");
+            this.lblError.BackColor = System.Drawing.Color.Transparent;
+            this.lblError.ForeColor = System.Drawing.Color.Red;
+            this.lblError.Name = "lblError";
+            // 
+            // panel4
+            // 
+            resources.ApplyResources(this.panel4, "panel4");
+            this.panel4.Name = "panel4";
             // 
             // pictureBox3
             // 
@@ -205,7 +140,6 @@ namespace CapaPresentacion
             // panel3
             // 
             this.panel3.BackColor = System.Drawing.Color.Transparent;
-            this.panel3.Controls.Add(this.label4);
             this.panel3.Controls.Add(this.labelContraseña);
             this.panel3.Controls.Add(this.lblError);
             this.panel3.Controls.Add(this.labelUsuario);
@@ -241,16 +175,29 @@ namespace CapaPresentacion
             resources.ApplyResources(this.txtusuario, "txtusuario");
             this.txtusuario.Name = "txtusuario";
             // 
+            // btnIniciar
+            // 
+            this.btnIniciar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(8)))), ((int)(((byte)(49)))), ((int)(((byte)(66)))));
+            resources.ApplyResources(this.btnIniciar, "btnIniciar");
+            this.btnIniciar.ForeColor = System.Drawing.Color.White;
+            this.btnIniciar.Name = "btnIniciar";
+            this.btnIniciar.UseVisualStyleBackColor = false;
+            this.btnIniciar.Click += new System.EventHandler(this.btnIniciar_Click);
+            // 
+            // gunaElipse3
+            // 
+            this.gunaElipse3.TargetControl = this;
+            // 
             // Login
             // 
             resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(8)))), ((int)(((byte)(49)))), ((int)(((byte)(66)))));
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnIniciar);
             this.Controls.Add(this.panel3);
-            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.pictureBox3);
-            this.Controls.Add(this.btnIniciar);
             this.Controls.Add(this.panel4);
             this.Controls.Add(this.panel1);
             this.ForeColor = System.Drawing.Color.OldLace;
@@ -258,15 +205,16 @@ namespace CapaPresentacion
             this.Name = "Login";
             this.Opacity = 0.95D;
             this.Load += new System.EventHandler(this.Login_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.btnCerrar)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
             this.panel3.ResumeLayout(false);
             this.panel3.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -274,17 +222,10 @@ namespace CapaPresentacion
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.PictureBox pictureBox2;
         private Guna.UI.WinForms.GunaElipse gunaElipse1;
-        private Bunifu.Framework.UI.BunifuElipse bunifuElipse1;
         private System.Windows.Forms.Panel panel2;
-        private Bunifu.Framework.UI.BunifuElipse bunifuElipse2;
         private Bunifu.Framework.UI.BunifuElipse bunifuElipse3;
-        private Bunifu.Framework.UI.BunifuFlatButton btnIniciar;
         private System.Windows.Forms.Label label4;
-        private Bunifu.Framework.UI.BunifuElipse bunifuElipse4;
-        private Guna.UI.WinForms.GunaElipse gunaElipse2;
-        private Guna.UI.WinForms.GunaElipse gunaElipse3;
         private System.Windows.Forms.PictureBox btnCerrar;
-        private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.Label lblError;
         protected System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Panel panel4;
@@ -294,5 +235,8 @@ namespace CapaPresentacion
         private System.Windows.Forms.Label labelUsuario;
         private System.Windows.Forms.TextBox txtcontraseña;
         private System.Windows.Forms.TextBox txtusuario;
+        private System.Windows.Forms.Button btnIniciar;
+        private Guna.UI.WinForms.GunaElipse gunaElipse2;
+        private Guna.UI.WinForms.GunaElipse gunaElipse3;
     }
 }
