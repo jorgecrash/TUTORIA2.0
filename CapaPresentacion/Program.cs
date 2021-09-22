97a82331858665d1dc5fcffade022d7f10719a7c
basto <75378141+eracles99@users.noreply.github.com>
2021-09-21 20:26:08 -0500

    rr

--- CapaPresentacion/Program.cs
+++ CapaPresentacion/Program.cs
@@ -16,7 +16,7 @@ namespace CapaPresentacion
         {
             //Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Login());
+            Application.Run(new FrmMain());
         }
     }
 }
